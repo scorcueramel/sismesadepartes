800a296554cf49f2275418498a9d6cc26dd264a6
scorcueramel <46231549+scorcueramel@users.noreply.github.com>
2022-05-25 23:27:52 -0500

    tercer commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //string length
+        Schema::defaultStringLength(191);
+        Paginator::useBootstrap();
     }
 }
--- config/app.php
+++ config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //proveedor de roles y permisos SPATIE
+        Spatie\Permission\PermissionServiceProvider::class,
     ],
 
     /*
